14b6ad252b7b020a9de5621c5d42570558e175bd
DKateryna <derkach.kateryna@gmail.com>
2017-08-14 17:41:38 +0200

    Fix api bookings create

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,7 +1,57 @@
 class Api::BookingsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def index
     render status: 200, json: {
       bookings: Booking.all
     }.to_json
   end
+
+  def show
+    booking = Booking.find(params[:id])
+
+    render status: 200, json: {
+      booking: booking
+    }.to_json
+  end
+
+  def create
+    booking = Booking.new(booking_params)
+
+    if booking.save
+      render status: 200, json: {
+        booking: booking
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: booking.errors
+      }.to_json
+    end
+
+    def taken
+      excluded_dates = []
+      Booking.upcoming_bookings.each do |booking|
+        date_from = booking.check_in
+        date_to = booking.check_out
+        date_array = (date_from...date_to).map(&:to_s)
+
+        date_array.each do |date|
+          excluded_dates << date
+        end
+      end
+      render status: 201, json: {
+        message: 'Booked Dates',
+        dates: excluded_dates
+      }.to_json
+    end
+    private
+
+    def booking_params
+      params.require(:booking).permit(:check_in, :check_out, :first_name,
+                                      :last_name, :title, :phone, :email, :street_name, :street_number,
+                                      :city, :zip_code, :people)
+    end
+  end
+
+
 end
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,42 +8,42 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
-  def create
-    booking = Booking.new(booking_params)
+  # def create
+  #   booking = Booking.new(booking_params)
+  #
+  #   if booking.save
+  #     render status: 200, json: {
+  #       booking: booking
+  #     }.to_json
+  #   else
+  #     render status: 422, json: {
+  #       errors: booking.errors
+  #     }.to_json
+  #   end
+  # end
 
-    if booking.save
-      render status: 200, json: {
-        booking: booking
-      }.to_json
-    else
-      render status: 422, json: {
-        errors: booking.errors
-      }.to_json
-    end
-  end
+  # def taken
+  #   excluded_dates = []
+  #   Booking.upcoming_bookings.each do |booking|
+  #     date_from = booking.check_in
+  #     date_to = booking.check_out
+  #     date_array = (date_from...date_to).map(&:to_s)
+  #
+  #     date_array.each do |date|
+  #       excluded_dates << date
+  #     end
+  #   end
+  #   render status: 201, json: {
+  #     message: 'Booked Dates',
+  #     dates: excluded_dates
+  #   }.to_json
+  # end
 
-  def taken
-    excluded_dates = []
-    Booking.upcoming_bookings.each do |booking|
-      date_from = booking.check_in
-      date_to = booking.check_out
-      date_array = (date_from...date_to).map(&:to_s)
-
-      date_array.each do |date|
-        excluded_dates << date
-      end
-    end
-    render status: 201, json: {
-      message: 'Booked Dates',
-      dates: excluded_dates
-    }.to_json
-  end
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:check_in, :check_out, :first_name,
-                                    :last_name, :title, :phone, :email, :street_name, :street_number,
-                                    :city, :zip_code, :people)
-  end
+  # private
+  #
+  # def booking_params
+  #   params.require(:booking).permit(:check_in, :check_out, :first_name,
+  #                                   :last_name, :title, :phone, :email, :street_name, :street_number,
+  #                                   :city, :zip_code, :people)
+  # end
 end
