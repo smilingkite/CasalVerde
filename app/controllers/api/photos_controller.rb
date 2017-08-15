600c30259129a2c3baa6216636b1276bd0300f60
DKateryna <derkach.kateryna@gmail.com>
2017-08-15 14:10:01 +0200

    Added api for photos

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -4,4 +4,33 @@ class Api::PhotosController < ApplicationController
       photos: Photo.all
     }.to_json
   end
+
+  def show
+    photo = Photo.find(params[:id])
+
+    render status: 200, json: {
+      photo: photo
+    }.to_json
+  end
+
+  def create
+    photo = Photo.new(photo_params)
+
+    if photo.save
+      render status: 201, json: {
+        message: 'Photo added',
+        photo: photo
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: photo.errors
+      }.to_json
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image, :photo_page_type, :photo_page_id)
+  end
 end
