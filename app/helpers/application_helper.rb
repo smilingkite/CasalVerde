79c3d54692db9e6f5f6772e6067d03ccb91fc772
smilingkite <mail@katinkahesselink.net>
2017-08-16 10:26:19 +0200

    add hardcoded titles to hardcoded pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def money(number)
     number_to_currency(number, unit: "€ ", separator: ",", delimiter: ".")
   end
+
+  def title(page_title)
+    content_for :title, page_title.to_s
+  end
 end
