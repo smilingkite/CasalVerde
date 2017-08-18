40f4393ee5bf0b768580362b5d714176a0a00e7d
Wouter de Vos <wouter@codaisseur.com>
2017-08-18 09:59:15 +0200

    Set up CORS

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,6 @@
 class Admin::BaseController < ApplicationController
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
   layout 'adminpanel'
 end
