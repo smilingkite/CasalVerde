f4c68bd404ba41e2cf9a2b2b4724bc6f9dc496dd
smilingkite <mail@katinkahesselink.net>
2017-08-18 10:29:50 +0200

    carousels controller not necessary

--- app/controllers/admin/carousels_controller.rb
+++ app/controllers/admin/carousels_controller.rb
@@ -1,4 +1,8 @@
 class Admin::CarouselsController < Admin::BaseController
+
+  # does NOT work right now. Needs route if we want it to work through the api
+  # and as it was before. 
+
   def index
     @page = Page.find(params[:page_id])
     render json: @page
